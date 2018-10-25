87a01be80b6bde7805c54e222d3dc7d1456b5dc0
mrybin <fellcross@rambler.ru>
2018-10-25 20:59:05 +0300

    Task 2

---
+++ sandbox/src/main/java/ru/fortesting/sandbox/Point.java
@@ -0,0 +1,13 @@
+package ru.fortesting.sandbox;
+
+public class Point {
+    public double x;
+    public double y;
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x-this.x)*(p2.x-this.x)+(p2.y-this.y)*(p2.y-this.y));
+    }
+    public Point(double x, double y) {
+        this.x=x;
+        this.y=y;
+    }
+}
--- sandbox/src/main/java/ru/fortesting/sandbox/first.java
+++ sandbox/src/main/java/ru/fortesting/sandbox/first.java
@@ -2,9 +2,14 @@ package ru.fortesting.sandbox;
 
 public class first {
 
-public static void main(String [] args) {
-
-    System.out.println("Test new program");
+    public static double distance (Point p1, Point p2) {
+        return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+    }
+    public static void main(String [] args) {
+        Point p1=new Point(1,2);
+        Point p2=new Point(4,6);
+        System.out.println("Result of function distance "+ distance(p1,p2));
+        System.out.println("Result of method distance "+ p1.distance(p2));
 }
 
 }
