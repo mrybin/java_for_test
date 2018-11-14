09cd43f29f2a58ba0a3272f8c8d9af3b32f7d9f2
mrybin <fellcross@rambler.ru>
2018-11-14 21:39:19 +0300

    4.7

--- addressbook-web-tests/src/test/java/ru/fortesting/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/fortesting/addressbook/tests/GroupModificationTests.java
@@ -4,12 +4,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.fortesting.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification(){
+
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("qwe", "asd", "zxc"));
@@ -17,10 +19,15 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("tes", "set", "zxc"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"qwe", "asd", "zxc");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() );
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object> (before),new HashSet<Object> (after));
     }
 }
