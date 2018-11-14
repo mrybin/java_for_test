ea33ec860e053dd8379e0f0b5e72009d741f32b9
mrybin <fellcross@rambler.ru>
2018-11-14 20:41:43 +0300

    4.5

--- addressbook-web-tests/src/test/java/ru/fortesting/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/fortesting/addressbook/appmanager/GroupHelper.java
@@ -3,7 +3,11 @@ package ru.fortesting.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.fortesting.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase{
 
@@ -58,5 +62,16 @@ public class GroupHelper extends HelperBase{
 
     public int getGroupCount() {
         return wd.findElements(By.name("selected[]")).size();
+    }
+
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("Span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            GroupData group =new GroupData(name,null,null);
+            groups.add(group);
+        }
+        return groups;
     }
 }
