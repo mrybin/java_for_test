59883494c002e5a35f9f3d7bfd39f9ed09ae5fb4
mrybin <fellcross@rambler.ru>
2018-11-15 21:18:38 +0300

    4.10

--- addressbook-web-tests/src/test/java/ru/fortesting/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/fortesting/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.fortesting.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -12,18 +14,18 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
       app.getNavigationHelper().gotoGroupPage();
       List<GroupData> before = app.getGroupHelper().getGroupList();
-      GroupData group = new GroupData("qwe", "asd", "zxc");
+      GroupData group = new GroupData("qwe1", "asd", "zxc");
       app.getGroupHelper().createGroup(group);
       List<GroupData> after = app.getGroupHelper().getGroupList();
       Assert.assertEquals(after.size(), before.size() + 1);
 
-      int max=0;
-      for (GroupData g: after) {
-          if (g.getId()>max)
-              max = g.getId();
-      }
-      group.setId(max);
+      group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+      Comparator<? super GroupData> byId=(g1, g2)-> Integer.compare(g1.getId(),g2.getId());
+      after.sort(byId);
+      group.setId(after.get(after.size()-1).getId());
       before.add(group);
+      before.sort(byId);
+      Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after) );
   }
 
 }
